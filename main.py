e1d2f6fb4b7b11e0fd0566804cdd7b192860f257
rafidika-ralali <rafidika.samekto@ralali.com>
2023-06-13 10:24:35 +0700

    feat: pubsub config

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.compat.v1 import graph_util
 import time
+import json
 from google.cloud import pubsub_v1
 from input import transform_input
 from output import transform_output
@@ -10,55 +11,60 @@ project_id = "medicure-bangkit23"
 publisher = pubsub_v1.PublisherClient()
 subscriber = pubsub_v1.SubscriberClient()
 
-# # subscribe
-# subscription_id = "submit-data-sub"
-# subscription_path = subscriber.subscription_path(project_id, subscription_id)
-# def callback(message):
-#     print(f"Received message: {message.data.decode('utf-8')}")
-#     message.ack()
-# subscriber.subscribe(subscription_path, callback=callback)
+def callback(message):
+    try:
+        # Path to the .tflite model file
+        model_path = './my_model.tflite'
 
-# print("Listening for messages...")
-# while True:
-#     time.sleep(5)
+        # Load the model
+        interpreter = tf.lite.Interpreter(model_path=model_path)
+        interpreter.allocate_tensors()
 
-# Path to the .tflite model file
-model_path = './my_model.tflite'
+        # Get input and output details
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
 
-# Load the model
-interpreter = tf.lite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
+        # Access the input and output tensors
+        input_tensor_index = input_details[0]['index']
+        output_tensor_index = output_details[0]['index']
+        input_tensor = interpreter.tensor(input_tensor_index)
+        output_tensor = interpreter.tensor(output_tensor_index)
 
-# Get input and output details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+        # Prepare your input data
+        json_string = json.dumps(message.data.decode('utf-8'))
+        print(json_string)
+        decoded_input = json.loads(json_string)
+        # Set the input tensor data
+        input_tensor()[0] = transform_input(decoded_input)
 
-# Access the input and output tensors
-input_tensor_index = input_details[0]['index']
-output_tensor_index = output_details[0]['index']
-input_tensor = interpreter.tensor(input_tensor_index)
-output_tensor = interpreter.tensor(output_tensor_index)
+        # Run the inference
+        interpreter.invoke()
 
-# TEST
-# Prepare your input data
-input_data = ['drying_and_tingling_lips',	'mucoid_sputum', 'diarrhea',	'chest_pain',	'phlegm',	'continuous_feel_of_urine',	'bruising']  # Prepare your input data as per the model requirements
+        # Get the output tensor data
+        output_data = output_tensor()[0]
 
-# Set the input tensor data
-input_tensor()[0] = transform_input(input_data)
+        # Transform output
+        output = transform_output(output_data)
+        result = {'disease': output}
+        dumped_result = json.dumps(result)
+        publish(dumped_result)
+        message.ack()
+    except:
+        print(message.data)
 
-# Run the inference
-interpreter.invoke()
+def publish(result):
+    # publish
+    topic_id = "predict"
+    topic_path = publisher.topic_path(project_id, topic_id)
+    future = publisher.publish(topic_path, data=result.encode("utf-8"))
+    message_id = future.result()
+    print(f"Published message ID: {message_id}")
 
-# Get the output tensor data
-output_data = output_tensor()[0]
+# subscribe
+subscription_id = "submit-data-sub"
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+subscriber.subscribe(subscription_path, callback=callback)
 
-output = transform_output(output_data)
-print(output)
-
-# publish
-topic_id = "predict"
-topic_path = publisher.topic_path(project_id, topic_id)
-message = b"Hello, Pub/Sub!"
-future = publisher.publish(topic_path, data=message)
-message_id = future.result()
-print(f"Published message ID: {message_id}")
+print("Listening for messages...")
+while True:
+    time.sleep(5)
